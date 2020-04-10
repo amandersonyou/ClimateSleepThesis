f01911c8ff4f406833610668c2402d63c0e13c46
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2020-04-10 13:11:34 -0500

    vue updates

---
+++ Code/script.js
@@ -0,0 +1,10 @@
+// create a new vue intance (root of app)
+// el connects to the html div with the id matching the el's assigned id (#app)
+// the data then points to what it holds to be loaded in the DOM. 
+var app = new Vue({
+    el: '#app',
+    data: {
+        mapTitle: 'USA Air Quality: Elemental Carbon',
+        image: '..Assets/testing_img.png'
+    }
+})
