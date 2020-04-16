d5d8ead12c7b694d4a3bfd9485e1e97a611afc20
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2020-04-16 17:28:17 -0500

    updates

--- Code/mainSketch2.js
+++ Code/mainSketch2.js
@@ -1,90 +1,93 @@
-// WIP if design is altered to be cube grid layout
+// WIP if design is altered to be full assortment
 
 // require https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.14/p5.js
 
 var sleepHigh
 var asthmaHigh
+var totalData
 
 function preload() {
-    sleepHigh = loadTable('/Data/500_Cities/SleepHigh.csv', 'csv', 'header')
-    asthmaHigh = loadTable('/Data/500_Cities/AsthmaHigh.csv', 'csv', 'header')
+    // sleepHigh = loadTable('/Data/500_Cities/SleepHigh.csv', 'csv', 'header')
+    // asthmaHigh = loadTable('/Data/500_Cities/AsthmaHigh.csv', 'csv', 'header')
+    totalData = loadTable('/Data/500_Cities/500_Cities__SleepAsthmaEPA.csv', 'csv', 'header')
 }
 
 function setup() {
-    let c = createCanvas(1000, 1000)
-    // background('#DBD3D1');
+    let c = createCanvas(3600, 700)
+    background('#DBD3D1');
     noStroke();
     noLoop();
 
     // colors
     var asthmaHue = "#e17e7b"
     var sleepHue = "#777a9e"
-    
 
 
-    var table_sleep = sleepHigh
-    var table_asthma = asthmaHigh
+    // var table_sleep = sleepHigh
+    // var table_asthma = asthmaHigh
+    var table_total = totalData
 
-    var bar_width = 40
+    var bar_width = 7
 
     // Top 15 records with highest lacking sleep percent prevelance
-    x = 170
-    y = 350
-    for (var r=0; r<table_sleep.getRowCount(); r++){
-        var aqiVal = (table_sleep.getNum(r, 4)*4)
-        fill(36,102,129, 140)
-        rect(x+bar_width, y, bar_width-15, -aqiVal)
-        x += bar_width
-    }
+
     
-    x = 200
+    x = 50
     y = 350
-    middle = 12
     fill(sleepHue)
-    for (var r=0; r<table_sleep.getRowCount(); r++){
-        var sleepVal = (table_sleep.getNum(r, 2)*4)
-        rect(x, y, bar_width-25, -sleepVal)
+    for (var r=0; r<table_total.getRowCount(); r++){
+        var sleepVal = (table_total.getNum(r, 2)*4)
+        rect(x, y, bar_width-2, -sleepVal)
         x += bar_width
     }
     
     // draw asthma bars per city
-    x = 180
+    x = 50
     y = 350
     fill(asthmaHue)
-    for (var r=0; r<table_sleep.getRowCount(); r++){
-        var asthmaVal = (table_sleep.getNum(r, 3)*4)
-        rect(x+bar_width, y, bar_width-25, -asthmaVal)
+    for (var r=0; r<table_total.getRowCount(); r++){
+        var asthmaVal = (table_total.getNum(r, 3)*4)
+        rect(x, y, bar_width-2, -asthmaVal)
         x += bar_width
     }
 
-    // Top 15 records with highest current adult asthma prevelance
-    x = 170
-    y = 650
-    for (var r=0; r<table_asthma.getRowCount(); r++){
-        var aqiVal = (table_asthma.getNum(r, 4)*4)
+    x = 50
+    y = 350
+    for (var r=0; r<table_total.getRowCount(); r++){
+        var aqiVal = (table_total.getNum(r, 4)*4)
         fill(36,102,129, 140)
-        rect(x+bar_width, y, bar_width-15, -aqiVal)
+        rect(x, y, bar_width-2, aqiVal)
         x += bar_width
     }
+
+    // // Top 15 records with highest current adult asthma prevelance
+    // x = 170
+    // y = 650
+    // for (var r=0; r<table_asthma.getRowCount(); r++){
+    //     var aqiVal = (table_asthma.getNum(r, 4)*4)
+    //     fill(36,102,129, 140)
+    //     rect(x+bar_width, y, bar_width-15, -aqiVal)
+    //     x += bar_width
+    // }
     
-    x = 200
-    y = 650
-    middle = 12
-    fill(sleepHue)
-    for (var r=0; r<table_asthma.getRowCount(); r++){
-        var sleepVal = (table_asthma.getNum(r, 2)*4)
-        rect(x, y, bar_width-25, -sleepVal)
-        x += bar_width
-    }
+    // x = 200
+    // y = 650
+    // middle = 12
+    // fill(sleepHue)
+    // for (var r=0; r<table_asthma.getRowCount(); r++){
+    //     var sleepVal = (table_asthma.getNum(r, 2)*4)
+    //     rect(x, y, bar_width-25, -sleepVal)
+    //     x += bar_width
+    // }
     
-    // draw asthma bars per city
-    x = 180
-    y = 650
-    fill(asthmaHue)
-    for (var r=0; r<table_sleep.getRowCount(); r++){
-        var asthmaVal = (table_asthma.getNum(r, 3)*4)
-        rect(x+bar_width, y, bar_width-25, -asthmaVal)
-        x += bar_width
-    }
-saveCanvas(c, 'Select15', 'png');
+    // // draw asthma bars per city
+    // x = 180
+    // y = 650
+    // fill(asthmaHue)
+    // for (var r=0; r<table_sleep.getRowCount(); r++){
+    //     var asthmaVal = (table_asthma.getNum(r, 3)*4)
+    //     rect(x+bar_width, y, bar_width-25, -asthmaVal)
+    //     x += bar_width
+    // }
+// saveCanvas(c, 'FullData_AsthmaUp', 'png');
 }
---
+++ Code/particles.js
@@ -0,0 +1,61 @@
+// P5 sketch for hero image of thesis
+
+let particles = [];
+
+function windowResized() {
+    resizeCanvas(windowWidth, 1000);
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, 1000);
+    canvas.position(0,0);
+    canvas.style('z-index', '-1');
+    frameRate(35);
+}
+
+function draw() {
+    background(220,212,209, 220);
+    for (let i = 0; i < 11; i ++) {
+        let p = new Particle();
+        particles.push(p);
+    }
+    for (let i = particles.length - 1; i >= 0; i--) {
+        particles[i].update();
+        particles[i].show();
+        if (particles[i].finished()) {
+            particles.splice(i, 1);
+        }
+    }
+}
+
+class Particle {
+
+    constructor() {
+        this.x = random(0, windowWidth);
+        this.y = 990;
+        this.vx = random(-1, 1);
+        this.vy = random(-7, -1);
+        this.alpha = 255;
+    }
+
+finished() {
+    return this.alpha < 0;
+}
+
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+        this.alpha-= 2;
+
+    }
+
+    show() {
+        noStroke();
+        // stroke(255);
+        fill(150, this.alpha);
+        ellipse(this.x, this.y, random(3, 10));
+    }
+}
+
+
+
